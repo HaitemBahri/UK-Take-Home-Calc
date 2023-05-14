c3cdae00e47c0a007019b84ddd4e8b69b2f1db70
HaitemBahri <haitem_bahri@yahoo.com>
2023-05-14 19:11:46 +0100

    Adding SalaryItem class unit tests

---
+++ UKTakeHomeCalc.Core.Test/ModelsTests/SalaryItemTests/SalaryItemTests_GetTotal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+using Xunit;
+
+namespace UKTakeHomeCalc.Core.Test.ModelsTests.SalaryItemTests
+{
+    public  class SalaryItemTests_GetTotal
+    {
+        [Theory]
+        [InlineData(39902.44, Frequency.ANNUALLY)]
+        [InlineData(-58.18, Frequency.WEEKLY)]
+        [InlineData(0, Frequency.MONTHLY)]
+        public void ShouldReturnItsOwnValue(decimal value, Frequency valueFrequency)
+        {
+            var expectedValue = new MonetaryValue(value, valueFrequency);
+            var sut = new SalaryItem("Random name", expectedValue);
+
+            var actualResult = sut.GetTotal();
+
+            Assert.Equal(expectedValue, actualResult);
+
+        }
+    }
+}
