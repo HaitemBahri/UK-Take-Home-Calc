05f3df6a4ec81cdbab7a28bc888a6fb4d05f4fba
HaitemBahri <haitem_bahri@yahoo.com>
2023-05-27 17:51:25 +0100

    overriding Equal and GetHashCode  methods in SalaryItem

---
+++ UKTakeHomeCalc.Core.Test/ModelsTests/SalaryItemTests/SalaryItemTests_ValueComparison.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+using Xunit;
+
+namespace UKTakeHomeCalc.Core.Test.ModelsTests.SalaryItemTests
+{
+    public class SalaryItemTests_ValueComparison
+    {
+        [Fact]
+        public void ShouldTwoSalaryItemsWithSameNameAndValueBeEqual()
+        {
+            var sut1 = new SalaryItem("some salaryItem", new MonetaryValue(52.2m, Frequency.WEEKLY));
+            var sut2 = new SalaryItem("some salaryItem", new MonetaryValue(52.2m, Frequency.WEEKLY));
+
+            Assert.Equal(sut1, sut2);
+        }
+
+        [Fact]
+        public void ShouldTwoSalaryItemsWithSameNameAndDifferentValueNotBeEqual()
+        {
+            var sut1 = new SalaryItem("some salaryItem", new MonetaryValue(455.7m, Frequency.MONTHLY));
+            var sut2 = new SalaryItem("some salaryItem", new MonetaryValue(52.2m, Frequency.MONTHLY));
+
+            Assert.NotEqual(sut1, sut2);
+        }
+
+        [Fact]
+        public void ShouldTwoSalaryItemsWithDifferentNameAndSameValueNotBeEqual()
+        {
+            var sut1 = new SalaryItem("some salaryItem - One", new MonetaryValue(52.2m, Frequency.ANNUALLY));
+            var sut2 = new SalaryItem("some salaryItem - Two", new MonetaryValue(52.2m, Frequency.ANNUALLY));
+
+            Assert.NotEqual(sut1, sut2);
+        }
+    }
+}
--- UKTakeHomeCalc.Core/Models/SalaryItem.cs
+++ UKTakeHomeCalc.Core/Models/SalaryItem.cs
@@ -20,5 +20,31 @@
 
             return null;
         }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            var value = ((SalaryItem)obj).Value;
+            var name = ((SalaryItem)obj).Name;
+
+            if (Value == value && Name == name)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int initialHash = 23;
+
+            unchecked
+            {
+                initialHash = initialHash * 23 + Value.GetHashCode();
+                initialHash = initialHash * 23 + Name.GetHashCode();
+            }
+
+            return initialHash;
+        }
     }
 }
