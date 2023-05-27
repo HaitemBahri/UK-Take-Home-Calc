0fe5de8dd14418f611bdfcbce93b79db5c94170e
HaitemBahri <haitem_bahri@yahoo.com>
2023-05-27 18:07:56 +0100

    adding BasicIncome class

---
+++ UKTakeHomeCalc.Core.Test/ModelsTests/GrossSalaryCalculatorTests/BasicIncomeTests/BasicIncomeTests_GetValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+using UKTakeHomeCalc.Core.Services.Calculator.GrossIncomeCalculator.IncomeItem;
+using Xunit;
+
+namespace UKTakeHomeCalc.Core.Test.ModelsTests.GrossSalaryCalculatorTests.BasicIncomeTests
+{
+    public class BasicIncomeTests_GetValue
+    {
+        [Fact]
+        public void ShouldReturnCorrectMonetaryValueWithSameInputFrequency()
+        {
+            var name = "my BasicIncome";
+            var value = 850.00m;
+            var freq = Frequency.WEEKLY;
+
+            var sut = new BasicIncome(name, value, freq);
+
+            var actualResult = sut.CreateSalaryItem();
+            var expectedResult = new SalaryItem(name, new MonetaryValue(value, freq));
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ UKTakeHomeCalc.Core/Services/Calculator/GrossIncomeCalculator/IncomeItem/BasicIncome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UKTakeHomeCalc.Core.Models;
+
+namespace UKTakeHomeCalc.Core.Services.Calculator.GrossIncomeCalculator.IncomeItem
+{
+    public class BasicIncome : IIncomeItem
+    {
+        private readonly MonetaryValue _monetaryValue;
+        private readonly string _name;
+
+        public BasicIncome(string name, decimal value, Frequency frequency)
+        {
+            _monetaryValue = new MonetaryValue(value, frequency);
+            _name = name;
+        }
+        public ISalaryItem CreateSalaryItem()
+        {
+            return new SalaryItem(_name, _monetaryValue);
+        }
+    }
+}
