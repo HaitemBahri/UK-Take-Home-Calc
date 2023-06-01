e263baa377538ed786f8b17742f578ab0a0604b9
haitembahri <haitem_bahri@yahoo.com>
2023-06-01 02:33:59 +0100

    adding IncomeLimitTaxableSalaryCalculationService

---
+++ UKTakeHomeCalc.Core/Services/Calculator/TaxCalculator/TaxableSalaryCalculationService/ITaxableSalaryCalculationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+
+namespace UKTakeHomeCalc.Core.Services.Calculator.TaxCalculator.TaxableSalaryCalculationService
+{
+    public interface ITaxableSalaryCalculationService
+    {
+        public MonetaryValue CalculateTaxableSalary(MonetaryValue salary);
+    }
+}
---
+++ UKTakeHomeCalc.Core/Services/Calculator/TaxCalculator/TaxableSalaryCalculationService/IncomeLimitTaxableSalaryCalculationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Helpers;
+using UKTakeHomeCalc.Core.Models;
+using UKTakeHomeCalc.Core.Services.Calculator.TaxCalculator.TaxFreeAllowance;
+
+namespace UKTakeHomeCalc.Core.Services.Calculator.TaxCalculator.TaxableSalaryCalculationService
+{
+    public class IncomeLimitTaxableSalaryCalculationService : ITaxableSalaryCalculationService
+    {
+        private readonly MonetaryValue INCOME_LIMIT = 100000m.Annually();
+        private readonly ITaxFreeAllowance _taxFreeAllowance;
+        public IncomeLimitTaxableSalaryCalculationService(ITaxFreeAllowance taxFreeAllowance)
+        {
+            _taxFreeAllowance = taxFreeAllowance;
+        }
+
+        public MonetaryValue CalculateTaxableSalary(MonetaryValue salary)
+        {
+            var initialTaxFreeAllowance = _taxFreeAllowance.GetTaxFreeAllowance();
+            var taxFreeAllowanceReduction = salary > INCOME_LIMIT ? (salary - INCOME_LIMIT) / 2 : 0;
+
+            var updatedTaxFreeAllowance = initialTaxFreeAllowance - taxFreeAllowanceReduction;
+            if (updatedTaxFreeAllowance < 0)
+                updatedTaxFreeAllowance = 0;
+
+            var taxableSalary = salary - updatedTaxFreeAllowance;
+            if(taxableSalary < 0)
+                taxableSalary = 0;
+
+            return taxableSalary;
+        }
+    }
+}
