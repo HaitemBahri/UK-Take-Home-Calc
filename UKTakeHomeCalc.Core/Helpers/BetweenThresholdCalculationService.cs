b400aedd85ec0a737378281cfe7f051f3150536b
haitembahri <haitem_bahri@yahoo.com>
2023-06-01 02:15:42 +0100

    adding ThresholdCalculationService class

---
+++ UKTakeHomeCalc.Core/Helpers/BetweenThresholdCalculationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Helpers.CalculationRules;
+using UKTakeHomeCalc.Core.Models;
+
+namespace UKTakeHomeCalc.Core.Helpers
+{
+    public class BetweenThresholdCalculationService
+    {
+        public MonetaryValue GetValuesBetweenThresholds(MonetaryValue value, MonetaryValue lowerThreshold, MonetaryValue upperThreshold)
+        {
+            if (value > upperThreshold)
+                return upperThreshold - lowerThreshold;
+
+            if (value < lowerThreshold)
+                return 0m;
+
+            return value - lowerThreshold;
+        }
+
+        public CalculationRuleResult CalculateValuesUsingCalculationRule(MonetaryValue value, CalculationRule calculationRule)
+        {
+            var results = new CalculationRuleResult();
+
+            foreach (var rule in calculationRule.Rules)
+            {
+                var valueInBetween = GetValuesBetweenThresholds(value, rule.FromValue, rule.ToValue);
+                var taxOnValueInBetween = valueInBetween * rule.Percentage;
+                results.AddResult(rule.Percentage, taxOnValueInBetween);
+            }
+
+            return results;
+        }
+    }
+}
