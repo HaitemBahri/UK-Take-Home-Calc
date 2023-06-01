960ceae0410e0e3dc4489831cb1a1b368e6271a1
haitembahri <haitem_bahri@yahoo.com>
2023-06-01 02:11:04 +0100

    adding extension methods to decimal that create MonetaryValues

---
+++ UKTakeHomeCalc.Core/Helpers/MonetaryValueExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+
+namespace UKTakeHomeCalc.Core.Helpers
+{
+    public static class MonetaryValueExtensions
+    {
+        public static MonetaryValue Weekly(this decimal value)
+        {
+            return new MonetaryValue(value, Frequency.WEEKLY);
+        }
+
+        public static MonetaryValue Monthly(this decimal value)
+        {
+            return new MonetaryValue(value, Frequency.MONTHLY);
+        }
+
+        public static MonetaryValue Annually(this decimal value)
+        {
+            return new MonetaryValue(value, Frequency.ANNUALLY);
+        }
+    }
+}
