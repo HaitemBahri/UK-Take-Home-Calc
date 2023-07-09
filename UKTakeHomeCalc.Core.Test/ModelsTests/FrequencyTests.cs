0749f504ce9536263a453821a93e557cfa52b49a
haitembahri <haitem_bahri@yahoo.com>
2023-07-10 00:40:56 +0100

    updating FrequencyTests

---
+++ UKTakeHomeCalc.Core.Test/ModelsTests/FrequencyTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UKTakeHomeCalc.Core.Models;
+using Xunit;
+
+namespace UKTakeHomeCalc.Core.Test.ModelsTests
+{
+    public class FrequencyTests
+    {
+        public static TheoryData<Frequency, int, string> ShouldConvertFrequencyToCorrectIntegerTheoryData =>
+            new()
+            {
+                {Frequency.Weekly, 7, "Weekly Value"},
+                {Frequency.Monthly, 30, "Monthly Value"},
+                {Frequency.Annually, 365, "Annually Value"},
+            };
+
+        [Theory]
+        [MemberData(nameof(ShouldConvertFrequencyToCorrectIntegerTheoryData))]
+        public void ShouldConvertFrequencyToCorrectInteger(Frequency value, int expectedResult, string testDataName)
+        {
+            var actualResult = (int)value;
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
