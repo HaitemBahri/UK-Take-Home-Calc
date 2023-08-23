1457bc22668884a598ca6bac3f422f21e794d0d9
haitembahri <haitem_bahri@yahoo.com>
2023-08-23 19:25:34 +0100

    adding example 2 to Demo

--- UKTakeHomeCalc.Demo/Program.cs
+++ UKTakeHomeCalc.Demo/Program.cs
@@ -2,6 +2,7 @@
 
 using UKTakeHomeCalc.Core.CalculationStrategies.DeductableStrategies.NationalInsuranceStrategies;
 using UKTakeHomeCalc.Core.CalculationStrategies.DeductableStrategies.PensionStrategies;
+using UKTakeHomeCalc.Core.CalculationStrategies.DeductableStrategies.StudentLoanStrategies;
 using UKTakeHomeCalc.Core.CalculationStrategies.DeductableStrategies.TaxStrategies;
 using UKTakeHomeCalc.Core.CalculationStrategies.IncomeStrategies;
 using UKTakeHomeCalc.Core.Calculators;
@@ -16,6 +17,8 @@ namespace UKTakeHomeCalc.Demo
         static void Main(string[] args)
         {
             Demo1();
+
+            Demo2();
         }
 
         static void Demo1()
@@ -91,6 +94,7 @@ namespace UKTakeHomeCalc.Demo
             //Printing the final Value
             var takeHomeValue = takeHomeSummary.GetTotal();
             Console.WriteLine($"\nTake-Home Value: {takeHomeValue.ConvertTo(Frequency.Weekly)} {takeHomeValue.ConvertTo(Frequency.Monthly)} {takeHomeValue.ConvertTo(Frequency.Annually)}");
+            Console.WriteLine();
         }
 
         static void Demo2()
@@ -99,8 +103,8 @@ namespace UKTakeHomeCalc.Demo
             /*  Example 1:
              *  
              *  Income:
-             *      -Hourly Rate Income: (15.5 an hour)/Weekly
-             *      -Overtime: 4 hours/Weekly
+             *      -Hourly Rate Income:  40 hours/Weekly @ (15.5 an hour)
+             *      -Overtime: 4 hours/Weekly @ (17.5 an hour)
              *      -Bonus: 150/Monthly
              *  
              *  Tax:
@@ -114,6 +118,60 @@ namespace UKTakeHomeCalc.Demo
              * 
              */
 
+
+            //Hourly Rate Income: (15.5 an hour)/Weekly
+            var hourlyRateIncomeStrategy = new HourlyRateIncomeStrategy("Hourly Rate Income", 15.5m, 40m, Frequency.Weekly);
+
+            //Overtime: 4 hours/Weekly @ (17.5 an hour)
+            var overtimeIncomeStrategy = new HourlyRateIncomeStrategy("Overtime Income", 17.5m, 4m, Frequency.Weekly);
+
+            //Bonus: 150/Monthly
+            var bonusIncomeStrategy = new BasicIncomeStrategy("Bonus Income", 150m, Frequency.Monthly);
+
+            //Creating a Calculator object to calculate total income
+            var incomeCalculator = new Calculator("Income", hourlyRateIncomeStrategy, overtimeIncomeStrategy, bonusIncomeStrategy);
+
+            //Plan 1 Student Loan
+            var plan1StudentLoanStrategy = new Plan1StudentLoanStrategy("Plan 1 Student Loan");
+
+            //Creating a Calculator object to calculate Student loan
+            var studentLoanCalculator = new Calculator("Student Loan", plan1StudentLoanStrategy);
+
+            //England Tax Rates - Blind person free allownace
+            var taxStrategy = new EnglandTaxStrategy("Tax (England)", FreeAllowances.Tax.BlindTaxFreeAllowance);
+
+            //Creating a Calculator object to calculate tax
+            var taxCalculator = new Calculator("Tax", taxStrategy);
+
+            //Class 1 National Insurance contributions
+            var nationalInsuranceStrategy = new ClassOneNationalInsuranceStrategy("Class 1", FreeAllowances.NationalInsurance.StandardNationalInsuranceFreeAllowance);
+
+            //Creating a Calculator object to calculate national insurance contributions
+            var nationalInsuranceCalculator = new Calculator("National Insurance", nationalInsuranceStrategy);
+
+            //CalculatorsHandler for 1st stage for calculations. Handles Income Calculator
+            var handler1 = new CalculatorsHandler(incomeCalculator);
+
+            //CalculatorsHandler for 2nd stage for calculations, Handles Student Loan, Tax and National Insurance Calculators simultaneously
+            var handler2 = new CalculatorsHandler(studentLoanCalculator, taxCalculator, nationalInsuranceCalculator);
+
+            ///Setting the order of the execution.
+            ///handler1 (1st stage) -> handler2 (2nd stage)
+            handler1.SetNext(handler2);
+
+            ///Preparing a TakeHomeSummary object that will be passed through the execution stages.
+            ///In each stage, the result from the calculation will be added to it.
+            var takeHomeSummary = new TakeHomeSummaryComposite("Example 2 Take-Home Summary");
+
+            //Starting the Take-Home calculation process.
+            handler1.Handle(takeHomeSummary);
+
+            //Printing detailed result of the Take-Home Summary
+            Console.WriteLine(takeHomeSummary.ToString());
+
+            //Printing the final Value
+            var takeHomeValue = takeHomeSummary.GetTotal();
+            Console.WriteLine($"\nTake-Home Value: {takeHomeValue.ConvertTo(Frequency.Weekly)} {takeHomeValue.ConvertTo(Frequency.Monthly)} {takeHomeValue.ConvertTo(Frequency.Annually)}");
         }
     }
 }
